48b5e2e67e0e66cf1733bfbdc2ab68af3856df1c
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-05 11:10:29 -0500

    INIT PROJECT.

---
+++ src/main/java/com/everisbootcamp/creditcard/CreditcardApplication.java
@@ -0,0 +1,13 @@
+package com.everisbootcamp.creditcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardApplication.class, args);
+	}
+
+}
